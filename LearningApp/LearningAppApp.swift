5800af010e9657517ddaf50a99b4f3c2b47fed51
Arthur <olehno.arturs@gmail.com>
2023-01-11 20:15:47 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Артур Олехно on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
